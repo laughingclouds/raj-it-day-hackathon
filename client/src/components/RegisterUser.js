69f4011ccae3351c917333e274464a91c59b0032
ARC4N3 <arcaneisc00l@gmail.com>
2023-03-21 05:19:36 +0530

    Registration form is now a modal

--- client/src/components/RegisterUser.js
+++ client/src/components/RegisterUser.js
@@ -1,87 +1,74 @@
-import { useState } from "react";
-
 export default function RegisterUser() {
-    const [hideForm, setHideForm] = useState({
-        bool: true,
-        txt: "Register?"
-    });
-
-    const toggleViewBtn = () => {
-        if (hideForm.bool) {
-            setHideForm({ bool: false, txt: "Cancel" })
-        } else {
-            setHideForm({ bool: true, txt: "Register?" })
-        }
-    }
     return (
         <>
-            <button onClick={toggleViewBtn}>{hideForm.txt}</button>
-
-            {
-                hideForm.bool ?
-                    <></>
-                    :
-                    <RegistrationForm />
-            }
+            <label htmlFor="registrationFormModal">Register?</label>
+            <input type="checkbox" id="registrationFormModal" className="modal-toggle" />
+            <RegistrationFormModal />
         </>
     );
 }
 
-function RegistrationForm() {
+function RegistrationFormModal() {
     return (
         <>
-            <h2>Register Yourself</h2>
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box relative">
+                    <label htmlFor="registrationFormModal" className="btn btn-sm btn-circle absolute right-2 top-2">X</label>
+                    <h2 className="font-bold">Register Yourself</h2>
 
-            <form action="/api/userpost" method="post" enctype="multipart/form-data">
-                <div>
-                    <label>ID</label>
-                    <input required id="id" name="id" type="text" />
-                </div>
+                    <form action="/api/userpost" method="post" enctype="multipart/form-data">
+                        <div>
+                            <label>ID</label>
+                            <input required id="id" name="id" type="text" />
+                        </div>
 
-                <div>
-                    <label>Name</label>
-                    <input required id="name" name="name" type="text" />
-                </div>
+                        <div>
+                            <label>Name</label>
+                            <input required id="name" name="name" type="text" />
+                        </div>
 
-                <div>
-                    <label>Address</label>
-                    <input required id="address" name="address" type="text" />
-                </div>
-                <div>
-                    <label>CGPA</label>
-                    <input required id="cgpa" name="cgpa" type="number" min={0} step={0.01} max={10}/>
-                </div>
+                        <div>
+                            <label>Address</label>
+                            <input required id="address" name="address" type="text" />
+                        </div>
+                        <div>
+                            <label>CGPA</label>
+                            <input required id="cgpa" name="cgpa" type="number" min={0} step={0.01} max={10} />
+                        </div>
 
-                <div>
-                    <label>10<sup>th</sup>%</label>
-                    <input required id="tenth" name="tenth" type="number" min={0} step={0.01} max={100} />
-                </div>
+                        <div>
+                            <label>10<sup>th</sup>%</label>
+                            <input required id="tenth" name="tenth" type="number" min={0} step={0.01} max={100} />
+                        </div>
 
-                <div>
-                    <label>12<sup>th</sup>%</label>
-                    <input required id="twelfth" name="twelfth" type="number" min={0} step={0.01} max={100}/>
-                </div>
+                        <div>
+                            <label>12<sup>th</sup>%</label>
+                            <input required id="twelfth" name="twelfth" type="number" min={0} step={0.01} max={100} />
+                        </div>
 
-                <div>
-                    <label>Remarks</label>
-                    <input required id="remarks" name="remarks" type="text" />
-                </div>
+                        <div>
+                            <label>Remarks</label>
+                            <input required id="remarks" name="remarks" type="text" />
+                        </div>
 
-                <div>
-                    <label>Phone</label>
-                    <input required id="phone" name="phone" type="text"/>
-                </div>
+                        <div>
+                            <label>Phone</label>
+                            <input required id="phone" name="phone" type="text" />
+                        </div>
 
-                <div>
-                    {[1, 2, 3, 4, 5].map(i =>
-                        <div key={i}>
-                            <label htmlFor={`img${i}`}>{`Img${i}`}</label>
-                            <input required id={`img${i}`} name={`img${i}`} type="file" />
-                        </div>)}
-                </div>
+                        <div>
+                            {[1, 2, 3, 4, 5].map(i =>
+                                <div key={i}>
+                                    <label htmlFor={`img${i}`}>{`Img${i}`}</label>
+                                    <input required id={`img${i}`} name={`img${i}`} type="file" />
+                                </div>)}
+                        </div>
 
-                <button>Submit</button>
-            </form>
+                        <button>Submit</button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
+
