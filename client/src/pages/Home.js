333af1630e70c7e045d68bf48982e159bd5ccf44
ARC4N3 <arcaneisc00l@gmail.com>
2023-03-21 05:19:19 +0530

    Spacing of the outermost component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 import DetailsNotFound from "../components/DetailsNotFound";
 import FileUpload from "../components/FileUpload";
-import RegisterUser from "../components/RegsiterUser";
+import RegisterUser from "../components/RegisterUser";
 import ShowDetails from "../components/ShowDetails";
 
 export default function Home() {
 
   return (
     <>
-      <h1>FaceCheck</h1>
+      <div className="grid place-items-center min-h-screen text-xl">
+        <div>
+          <header className="">
+            <h1 className="text-4xl font-extrabold">FaceCheck</h1>
+          </header>
 
-      <FileUpload />
+          <div className="flex flex-col">
+            <FileUpload />
 
-      <Routes>
-        <Route path="/:id" element={<ShowDetails />} />
-        <Route path="/None" element={<DetailsNotFound />} />
-      </Routes>
-      
-      <RegisterUser />
+            <Routes>
+              <Route path="/:id" element={<ShowDetails />} />
+              <Route path="/None" element={<DetailsNotFound />} />
+            </Routes>
+
+            <RegisterUser />
+          </div>
+        </div>
+
+      </div>
     </>
   );
 }
