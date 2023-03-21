b6356b3efc50468640b421deda70178298434b09
ARC4N3 <arcaneisc00l@gmail.com>
2023-03-21 06:01:15 +0530

    Styling for user registration

--- client/src/components/RegisterUser.js
+++ client/src/components/RegisterUser.js
@@ -1,7 +1,7 @@
 export default function RegisterUser() {
     return (
         <>
-            <label htmlFor="registrationFormModal">Register?</label>
+            <label htmlFor="registrationFormModal" className="font-bold">Register?</label>
             <input type="checkbox" id="registrationFormModal" className="modal-toggle" />
             <RegistrationFormModal />
         </>
@@ -17,55 +17,25 @@ function RegistrationFormModal() {
                     <h2 className="font-bold">Register Yourself</h2>
 
                     <form action="/api/userpost" method="post" enctype="multipart/form-data">
-                        <div>
-                            <label>ID</label>
-                            <input required id="id" name="id" type="text" />
-                        </div>
-
-                        <div>
-                            <label>Name</label>
-                            <input required id="name" name="name" type="text" />
-                        </div>
-
-                        <div>
-                            <label>Address</label>
-                            <input required id="address" name="address" type="text" />
-                        </div>
-                        <div>
-                            <label>CGPA</label>
-                            <input required id="cgpa" name="cgpa" type="number" min={0} step={0.01} max={10} />
-                        </div>
-
-                        <div>
-                            <label>10<sup>th</sup>%</label>
-                            <input required id="tenth" name="tenth" type="number" min={0} step={0.01} max={100} />
-                        </div>
-
-                        <div>
-                            <label>12<sup>th</sup>%</label>
-                            <input required id="twelfth" name="twelfth" type="number" min={0} step={0.01} max={100} />
-                        </div>
-
-                        <div>
-                            <label>Remarks</label>
-                            <input required id="remarks" name="remarks" type="text" />
-                        </div>
-
-                        <div>
-                            <label>Phone</label>
-                            <input required id="phone" name="phone" type="text" />
-                        </div>
-
+                        <input type="text" placeholder="ID" required id="id" name="id" className="input input-bordered input-accent w-full mt-2 max-w-xs" />
+                        <input type="text" placeholder="Name" required id="name" name="name" className="input input-bordered input-accent w-full mt-2 max-w-xs" />
+                        <input type="text" placeholder="Address" required id="address" name="address" className="input input-bordered input-accent w-full mt-2 max-w-xs" />
+                        <input type="number" placeholder="CGPA" required id="cgpa" name="cgpa" min={0} step={0.01} max={10} className="input input-bordered input-accent w-full mt-2 max-w-xs" />
+                        <input placeholder="10th percentage" required id="tenth" name="tenth" type="number" min={0} step={0.01} max={100} className="input input-bordered input-accent w-full mt-2 max-w-xs" />
+                        <input placeholder="12th percentage" required id="twelfth" name="twelfth" type="number" min={0} step={0.01} max={100} className="input input-bordered input-accent w-full mt-2 max-w-xs" />
+                        <input placeholder="Remarks" required id="remarks" name="remarks" type="text" className="input input-bordered input-accent w-full mt-2 max-w-xs" />
+                        <input type="text" placeholder="Phone" required id="phone" name="phone" className="input input-bordered input-accent w-full mt-2 max-w-xs" />
                         <div>
                             {[1, 2, 3, 4, 5].map(i =>
                                 <div key={i}>
-                                    <label htmlFor={`img${i}`}>{`Img${i}`}</label>
-                                    <input required id={`img${i}`} name={`img${i}`} type="file" />
+
+                                    <label className="label" htmlFor={`img${i}`}><span className="label-text">{`Image ${i}`}</span></label>
+                                    <input required id={`img${i}`} name={`img${i}`} type="file" className="file-input file-input-bordered" />
                                 </div>)}
                         </div>
 
-                        <button>Submit</button>
-                    </form>
+                        <button className="btn mt-4">Submit</button>
+                    </form> 
                 </div>
             </div>
         </>
